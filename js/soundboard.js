cc60f07576a40b011ec6026a3e79f3c196d09fe9
perryharlock <perry.harlock@tiscali.co.uk>
2013-01-10 15:20:01 +0000

    Removed soundboard

--- js/soundboard.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-	$("audio").removeAttr("controls").each(function(i, audioElement) {
-	    var audio = $(this);
-	    var that = this;
-	    $("#doc").append($('<li><a class="'+audio.attr("class")+'" href="#" title="'+audio.attr("title")+'"><img src="img/' + audio.attr("class") + '.png"/></a></li>').click(function() {that.play();}));
-	});
-
-});
