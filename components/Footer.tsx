6a143f1c6eb2f29acbcc014933189a9b27178823
Perry Harlock <perry@hussle.com>
2025-05-22 12:44:45 +0100

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,41 +1,34 @@
 import { twMerge } from "tailwind-merge";
 
 export const Footer = () => {
-  const links = [
-    {
-      href: "https://uk.linkedin.com/pub/perry-harlock/12/1b7/170",
-      text: "LinkedIn",
-      title: "Find out about me on LinkedIn",
-    },
-    {
-      href: "https://github.com/perryharlock",
-      text: "GitHub",
-      title: "Check me out on GitHub",
-    },
+  const socialClasses = ["text-center", "mb-[60px]", "max-w-[320px]", "mx-auto", "md:mb-[150px]", "md:max-w-none"];
+  const linkClasses = [
+    "w-[90px]",
+    "h-[90px]",
+    "block",
+    "bg-no-repeat",
+    "bg-center",
+    "bg-[length:70%]",
+    "transition-colors",
+    "duration-300",
+    "ease-linear",
+    "clip-path-supported-h-100",
+    "clip-hexagon",
+    "hover:bg-yellow-500",
   ];
 
   return (
-    <footer className="footer" id="contact">
-      <div className="container">
+    <footer
+      className="bg-blue-500 text-grey-900 relative pt-[120px] md:pt-[200px] pb-[20px] footer-top md:footer-top-md lg:footer-top-lg"
+      id="contact"
+    >
+      <div className="container mx-auto">
         <div className="row">
           <div className="col-md-24">
-            <ul className="list-inline footer__social">
-              {links.map((link) => (
-                <li key={link.text}>
-                  <a
-                    className={twMerge("footer__link", `footer__link--${link.text.toLowerCase()}`)}
-                    target="_blank"
-                    rel="me noreferrer"
-                    href={link.href}
-                    title={link.title}
-                  >
-                    <span className="sr-only">{link.text}</span>
-                  </a>
-                </li>
-              ))}
-              <li>
+            <ul className={twMerge(socialClasses)}>
+              <li className="mr-[25px] md:mr-[30px] inline-block">
                 <a
-                  className="footer__link footer__link--linkedin"
+                  className={twMerge(linkClasses, "bg-linkedin bg-[length:60%]")}
                   target="_blank"
                   rel="me noreferrer"
                   href="https://uk.linkedin.com/pub/perry-harlock/12/1b7/170"
@@ -44,9 +37,9 @@ export const Footer = () => {
                   <span className="sr-only">LinkedIn</span>
                 </a>
               </li>
-              <li>
+              <li className="md:ml-[30px] inline-block">
                 <a
-                  className="footer__link footer__link--github"
+                  className={twMerge(linkClasses, "bg-github")}
                   target="_blank"
                   rel="me noreferrer"
                   href="https://github.com/perryharlock"
@@ -56,11 +49,14 @@ export const Footer = () => {
                 </a>
               </li>
             </ul>
-            <div className="text-center font-semi">&copy; Perry Harlock, 2025</div>
+            <div className="text-center font-semibold">&copy; Perry Harlock, 2025</div>
           </div>
         </div>
       </div>
-      <a href="#top" className="footer__scroll js-nav__link hidden-xs">
+      <a
+        href="#top"
+        className="absolute hidden md:inline-block left-0 right-0 bottom-[100px] w-full h-[46px] bg-center bg-no-repeat bg-scroll-up animate-pulse"
+      >
         <span className="sr-only">Click to jump to the top of this page</span>
       </a>
     </footer>
