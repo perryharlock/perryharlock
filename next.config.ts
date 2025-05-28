5b88758d8f0e6f38ae9ffc4612f92599ef5ff645
Perry Harlock <perry@hussle.com>
2025-05-28 13:52:43 +0100

    fix build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
+const isExport = process.env.NEXT_EXPORT === "true";
+
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
+  output: isExport ? "export" : undefined,
 };
 
 export default nextConfig;
