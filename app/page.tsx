8155f7b81c3fcf8df36080ab7f566cdf8bbad951
Perry Harlock <perry@hussle.com>
2025-05-28 13:24:19 +0100

    make static and update deploy.yml

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import { PortfolioSection } from "@/components/PortfolioSection";
 import { CVSection } from "@/components/CVSection";
 import { TestimonialsSection } from "@/components/TestimonialsSection";
 
-export const dynamic = "force-dynamic";
-
 export default function Home() {
   return (
     <main>
